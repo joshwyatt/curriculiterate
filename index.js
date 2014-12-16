945ae14b67f17716719ea7ba25c0822234b233bb
joshWyatt <joshuawyatt123@gmail.com>
2014-12-16 11:47:34 -0800

    Create new task assigned to me with title and comments that arrive in my inbox

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var request = require('request');
 
 var asanaAPIKey = process.env.ASANA_KEY_64;
-var organization = process.env.ASANA_ORG_ID;
+var workspaceID = process.env.ASANA_ORG_ID;
 var assignee = process.env.ASANA_ASSIGNEE_EMAIL;
 
-var baseURL = 'https://app.asana.com/api/1.0/users/me';
+var baseURL = 'https://app.asana.com/api/1.0/workspaces/' + workspaceID + '/tasks/';
+
 var requestOptions = {
+  method: 'POST',
   url: baseURL,
+  headers: {
+    'Authorization': 'Basic ' + asanaAPIKey
+  },
+  json: true,
+  body: {
+    data: {
+      name: 'TESTING FROM CURRICULITERATE 1',
+      notes: 'this is a test as i develop curriculiterate, sorry if it\'s in your way!',
+      assignee_status: 'inbox',
+      assignee: assignee
+    }
+  }
+};
+
+//THIS REQUEST OPTIONS IS JUST FOR PLAYING AROUND WITH DURING DEVELOPMENT
+var testRequestOptions = {
+  method: 'GET',
+  url: 'https://app.asana.com/api/1.0/workspaces',
   headers: {
     'Authorization': 'Basic ' + asanaAPIKey
   }
@@ -18,6 +38,6 @@ function requestCallback(err, res, body){
   if( err ){
     console.log('there was an error: ', err);
   }else{
-    console.log(JSON.parse(body));
+    console.log(body);
   }
 }
