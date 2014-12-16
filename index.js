58bf15a744806177b4c0788cc362bfc219171022
joshWyatt <joshuawyatt123@gmail.com>
2014-12-16 11:57:28 -0800

    Refactor into iffe and use strict

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-var request = require('request');
+;(function(){
+  'use strict';
+  var request = require('request');
 
-var asanaAPIKey = process.env.ASANA_KEY_64;
-var workspaceID = process.env.ASANA_ORG_ID;
-var assignee = process.env.ASANA_ASSIGNEE_EMAIL;
+  var asanaAPIKey = process.env.ASANA_KEY_64;
+  var workspaceID = process.env.ASANA_ORG_ID;
+  var assignee = process.env.ASANA_ASSIGNEE_EMAIL;
 
-var baseURL = 'https://app.asana.com/api/1.0/workspaces/' + workspaceID + '/tasks/';
+  var baseURL = 'https://app.asana.com/api/1.0/workspaces/' + workspaceID + '/tasks/';
 
-var requestOptions = {
-  method: 'POST',
-  url: baseURL,
-  headers: {
-    'Authorization': 'Basic ' + asanaAPIKey
-  },
-  json: true,
-  body: {
-    data: {
-      name: 'TESTING FROM CURRICULITERATE 1',
-      notes: 'this is a test as i develop curriculiterate, sorry if it\'s in your way!',
-      assignee_status: 'inbox',
-      assignee: assignee
+  var requestOptions = {
+    method: 'POST',
+    url: baseURL,
+    headers: {
+      'Authorization': 'Basic ' + asanaAPIKey
+    },
+    json: true,
+    body: {
+      data: {
+        name: 'TESTING FROM CURRICULITERATE 1',
+        notes: 'this is a test as i develop curriculiterate, sorry if it\'s in your way!',
+        assignee_status: 'inbox',
+        assignee: assignee
+      }
+    }
+  };
+
+  //THIS REQUEST OPTIONS IS JUST FOR PLAYING AROUND WITH DURING DEVELOPMENT
+  var testRequestOptions = {
+    method: 'GET',
+    url: 'https://app.asana.com/api/1.0/workspaces',
+    headers: {
+      'Authorization': 'Basic ' + asanaAPIKey
+    }
+  };
+
+  request(requestOptions, requestCallback);
+
+  function requestCallback(err, res, body){
+    if( err ){
+      console.log('there was an error: ', err);
+    }else{
+      console.log(body);
     }
   }
-};
-
-//THIS REQUEST OPTIONS IS JUST FOR PLAYING AROUND WITH DURING DEVELOPMENT
-var testRequestOptions = {
-  method: 'GET',
-  url: 'https://app.asana.com/api/1.0/workspaces',
-  headers: {
-    'Authorization': 'Basic ' + asanaAPIKey
-  }
-};
-
-request(requestOptions, requestCallback);
-
-function requestCallback(err, res, body){
-  if( err ){
-    console.log('there was an error: ', err);
-  }else{
-    console.log(body);
-  }
-}
+})();
