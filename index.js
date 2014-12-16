a500d66e0ef685f3204bbfcc65ed74cd3a1486c3
joshWyatt <joshuawyatt123@gmail.com>
2014-12-16 10:41:40 -0800

    Interpolate api_key

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-var asanaAPIKey = process.env.ASANA_API_KEY;
+var asanaAPIKey = process.env.ASANA_KEY_64;
 var organization = process.env.ASANA_ORG_ID;
 var assignee = process.env.ASANA_ASSIGNEE_EMAIL;
 
@@ -8,7 +8,7 @@ var baseURL = 'https://app.asana.com/api/1.0/users/me';
 var requestOptions = {
   url: baseURL,
   headers: {
-    'Authorization': 'Basic NHRnMkc4b2MublhWYXdJOWtxSHY3NTVBT245cXZHWGo6'
+    'Authorization': 'Basic ' + asanaAPIKey
   }
 };
 
