2e1e233f75c8d0b1dd0afbf41f474588a6e2ce9a
joshWyatt <joshuawyatt123@gmail.com>
2014-12-17 11:00:38 -0800

    Add proper shebang

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+#!/usr/bin/env node
+
 'use strict';
+
 var prompt = require('prompt');
 var sendToAsana = require('./send_ticket_to_asana.js');
 var ticket = {};
+var ticketKeys = ['RepoName', 'HIRName', 'Email', 'ExtendedNotes']
 
 prompt.start();
 
-prompt.addProperties(ticket, ['RepoName', 'HIRName', 'Email', 'ExtendedNotes'], promptHandler);
+prompt.addProperties(ticket, ticketKeys, promptHandler);
 
 function promptHandler(err, result){
-  // console.log('RepoName: ' + result.RepoName);
-  // console.log('HIRName: ' + result.HIRName);
-  // console.log('Email: ' + result.Email);
-  // console.log('ExtendedNotes: ' + result.ExtendedNotes);
   sendToAsana(ticket);
 }
