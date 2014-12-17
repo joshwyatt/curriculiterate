c4ad3dcd3aec77c89f29aedb833f66a69e53b107
joshWyatt <joshuawyatt123@gmail.com>
2014-12-16 17:29:38 -0800

    Remove iffe wrapper for use as npm module

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-(function(){
-  'use strict';
-  var prompt = require('prompt');
-  var sendToAsana = require('./send_ticket_to_asana.js');
-  var ticket = {};
+'use strict';
+var prompt = require('prompt');
+var sendToAsana = require('./send_ticket_to_asana.js');
+var ticket = {};
 
-  prompt.start();
+prompt.start();
 
-  prompt.addProperties(ticket, ['RepoName', 'HIRName', 'Email', 'ExtendedNotes'], promptHandler);
+prompt.addProperties(ticket, ['RepoName', 'HIRName', 'Email', 'ExtendedNotes'], promptHandler);
 
-  function promptHandler(err, result){
-    // console.log('RepoName: ' + result.RepoName);
-    // console.log('HIRName: ' + result.HIRName);
-    // console.log('Email: ' + result.Email);
-    // console.log('ExtendedNotes: ' + result.ExtendedNotes);
-    sendToAsana(ticket);
-  }
-
-})();
-
-//NOTES
-//what does this dialogue need to look like?
-//1) name of the repo
-//2) name of contact point
-//3) email of contact point
-//4) notes on what needs perhaps to change
+function promptHandler(err, result){
+  // console.log('RepoName: ' + result.RepoName);
+  // console.log('HIRName: ' + result.HIRName);
+  // console.log('Email: ' + result.Email);
+  // console.log('ExtendedNotes: ' + result.ExtendedNotes);
+  sendToAsana(ticket);
+}
--- send_ticket_to_asana.js
+++ send_ticket_to_asana.js
@@ -1,44 +1,41 @@
-(function(){
-  'use strict';
-  module.exports = function(ticket){
-    var request = require('request');
+'use strict';
+module.exports = function(ticket){
+  var request = require('request');
 
-    var asanaAPIKey = process.env.ASANA_KEY_64;
-    var workspaceID = process.env.ASANA_ORG_ID;
-    var assignee = process.env.ASANA_ASSIGNEE_EMAIL;
-    var repoName = ticket.RepoName;
-    var HIRName = ticket.HIRName;
-    var HIRemail = ticket.Email;
-    var extendedNotes = ticket.ExtendedNotes;
+  var asanaAPIKey = process.env.ASANA_KEY_64;
+  var workspaceID = process.env.ASANA_ORG_ID;
+  var assignee = process.env.ASANA_ASSIGNEE_EMAIL;
+  var repoName = ticket.RepoName;
+  var HIRName = ticket.HIRName;
+  var HIRemail = ticket.Email;
+  var extendedNotes = ticket.ExtendedNotes;
 
-    var baseURL = 'https://app.asana.com/api/1.0/workspaces/' + workspaceID + '/tasks/';
+  var baseURL = 'https://app.asana.com/api/1.0/workspaces/' + workspaceID + '/tasks/';
 
-    var requestOptions = {
-      method: 'POST',
-      url: baseURL,
-      headers: {
-        'Authorization': 'Basic ' + asanaAPIKey
-      },
-      json: true,
-      body: {
-        data: {
-          name: 'Iterate on ' + repoName + ' from ' + HIRName + ' - ' + HIRemail,
-          notes: extendedNotes,
-          assignee_status: 'inbox',
-          assignee: assignee
-        }
-      }
-    };
-
-    function requestCallback(err, res, body){
-      if( err ){
-        console.log('there was an error: ', err);
-      }else{
-        console.log(body);
+  var requestOptions = {
+    method: 'POST',
+    url: baseURL,
+    headers: {
+      'Authorization': 'Basic ' + asanaAPIKey
+    },
+    json: true,
+    body: {
+      data: {
+        name: 'Iterate on ' + repoName + ' from ' + HIRName + ' - ' + HIRemail,
+        notes: extendedNotes,
+        assignee_status: 'inbox',
+        assignee: assignee
       }
     }
-
-    request(requestOptions, requestCallback);
   };
 
-})();
+  function requestCallback(err, res, body){
+    if( err ){
+      console.log('there was an error: ', err);
+    }else{
+      console.log(body);
+    }
+  }
+
+  request(requestOptions, requestCallback);
+};
